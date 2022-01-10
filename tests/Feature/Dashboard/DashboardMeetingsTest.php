2c2fdb1497b5418fd1d7c4e9e4e1d8cd01f26b28
Maria <marcasbon@alum.es>
2022-01-10 20:12:14 +0100

    <title>: Fixed Meetings Dashboard test
    <type>: Test, backend.
    <body>: Manor changes or meetings dashboard.

--- tests/Feature/Dashboard/DashboardMeetingsTest.php
+++ tests/Feature/Dashboard/DashboardMeetingsTest.php
@@ -9,7 +9,7 @@ class DashboardMeetingsTest extends TestCase
     public function test_meeting_dashboard_view(){
 
         $meetings_count = 13;
-        $meetings_by_committee = array(array('comittee_id' => 'CommitteTest', 'total' => 2),array('comittee_id' => 'CommitteTest2', 'total' => 3)
+        $meeting_by_commitee = array(array('comittee_id' => 'CommitteTest', 'total' => 2),array('comittee_id' => 'CommitteTest2', 'total' => 3)
         ,array('comittee_id' => 'CommitteTest3', 'total' => 3,array('comittee_id' => 'CommitteTest4', 'total' => 3),array('comittee_id' => 'CommitteTest7', 'total' => 2)));
         $comittee_names_json = json_encode(array('CommitteTest','CommitteTest2','CommitteTest3'));
         $committee_values_json = json_encode(array(2,3,3,3,2));
@@ -17,13 +17,13 @@ class DashboardMeetingsTest extends TestCase
 
         $view = $this->view('dashboard.meetings', ['meetings' => [
             'meetings_count' => $meetings_count,
-            'meetings_by_commitee' => $meetings_by_committee,
+            'meeting_by_commitee' => $meeting_by_commitee,
             'comittee_names_json' => $comittee_names_json,
             'comittee_values_json' => $committee_values_json
         ]]);
         $view->assertSee($meetings_count);
 
-        foreach ($meetings_by_committee as $meeting_commitee) {
+        foreach ($meeting_by_commitee as $meeting_commitee) {
             $view->assertSee($meeting_commitee['comittee_id']);
             $view->assertSee($meeting_commitee['total']);
         }
